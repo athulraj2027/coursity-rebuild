a175c476efa5646c213eae3f94ccc38d7297f06b
athulraj2027 <athulraj2027@gmail.com>
2026-02-25 23:39:50 +0530

    modified proxy

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -10,19 +10,17 @@ type JWTPayload = {
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const token = request.cookies.get("auth_token")?.value ;
+  const token = request.cookies.get("auth_token")?.value;
   console.log("token received : ", token);
 
   if (pathname.startsWith("/lecture")) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+    if (!token) return NextResponse.redirect(new URL("/sign-in", request.url));
     return NextResponse.next();
   }
 
-  if (!token && !PUBLIC_ROUTES.includes(pathname)) {
+  if (!token && !PUBLIC_ROUTES.includes(pathname))
     return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+
   if (token) {
     const decoded = jwtDecode<JWTPayload>(token);
     console.log("decoded token : ", decoded);
