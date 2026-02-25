8a93515511a2f6f4a072b96f9762b087855f880d
athulraj2027 <athulraj2027@gmail.com>
2026-02-25 22:25:47 +0530

    removal token modified

--- http/src/controllers/auth.controller.ts
+++ http/src/controllers/auth.controller.ts
@@ -68,7 +68,9 @@ const AuthController = {
       res.clearCookie("auth_token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        domain: process.env.DOMAIN_URL,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
       return res.status(200).json({
